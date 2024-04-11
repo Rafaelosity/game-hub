10b16dfb6c16898e074efdd470d25446f0c2d3c6
Rafael B Guedelha <122017123+Rafaelosity@users.noreply.github.com>
2024-04-11 08:49:35 +0200

    create a custom hook to fetch the games

---
+++ src/assets/hooks/useGames.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import apiClient from "../../components/services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const controller = new AbortController(); // Moved inside useEffect
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return; // This check is crucial to avoid setting state after unmount
+        setError(err.message);
+      });
+
+    return () => controller.abort(); // Cleanup function to abort the request on unmount
+  }, []); // Empty dependency array means this runs once on mount
+
+  return { games, error };
+};
+
+export default useGames;
